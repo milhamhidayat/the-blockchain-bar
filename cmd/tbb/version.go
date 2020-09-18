f5422975909c7ac6402f8f06540ce9b1f0d2e648
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2020-09-18 22:47:46 +0700

    feat: add initial cli

---
+++ cmd/tbb/version.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	// Major is app major versioning
+	Major = "0"
+	// Minor is app minor versioning
+	Minor = "1"
+	// Fix is app fix versioning
+	Fix = "0"
+	// Verbal is app information
+	Verbal = "TX Add && Balances List"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Describes version.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Version: %s.%s.%s-beta %s", Major, Minor, Fix, Verbal)
+	},
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module the-blockchain-bar
 
 go 1.15
+
+require github.com/spf13/cobra v1.0.0
